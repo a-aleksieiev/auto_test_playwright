0489cc982ca2713558962606ef7733e13db9e0d7
a-aleksieiev <unxn226@gmail.com>
2024-04-23 00:48:46 +0300

    moreTests

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,11 +1,22 @@
-const websiteName = 'https://tv.kyivstar.ua';
-export async function login(page){
+const websiteName = 'https://webng.dev.vidmind.com/ua/';
+export async function loginFTTB(page){
     await page.goto(websiteName);
     await page.click("text=Увійти");
     await page.click("text=Особовий рахунок");
-    await page.type("input[type=text]", "demo_alk");
-    await page.type("input[type=password]", "1234567");
-    await page.click('//*[@id="cdk-overlay-0"]/vd-overlay-modal/div/vd-base-authorization/div[4]/button'); // Змінив селектор тут
+    await page.type("input[type=text]", "t300");
+    await page.type("input[type=password]", "123456");
+    await page.keyboard.press('Enter'); 
+    await page.click('//*[@id="cdk-overlay-1"]/vd-overlay-modal/div/vd-choose-profile/div/div/div[1]/div[2]/div/div[1]');
+}
+
+export async function loginMSISDN(page) {
+    const loginButton = page.locator('//*[@id="cdk-overlay-2"]/vd-overlay-modal/div/vd-base-authorization/div[3]/button');
+    await page.goto(websiteName);
+    await page.click("text=Увійти");
+    await page.type("input[type=tel]", "0688758389");
+    await page.click('.ks-btn__primary.w-full >> text=Далі');
+    await page.type("input[type=text]", "1111");
+    await page.keyboard.press('Enter');
     await page.click('//*[@id="cdk-overlay-1"]/vd-overlay-modal/div/vd-choose-profile/div/div/div[1]/div[2]/div/div[1]');
 }
 
@@ -17,7 +28,21 @@ export async function search(page) {
 
 export async function chips(page) {
     await page.click('.ks-btn__ghost >> text=Доступно');
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(3000);
     await page.click('.ks-btn__ghost >> text=За підпискою');
+    await page.waitForTimeout(2000);
+}
+
+export async function buy(page) {
+    const clickLocator = page.locator("text=Покупка"); 
+    await clickLocator.first().click();
+    await page.click(".ks-btn__primary");
+    await page.click(".ng-star-inserted >> text=Купити в SD якості");
+    await page.click("text=444433******1111");
     await page.waitForTimeout(1000);
+    await page.click(".payment__btn.btn.btn-primary");
+    await page.click(".ks-btn__primary.w-full >> text=Почати перегляд");
+    await page.waitForTimeout(1000);
+    await page.click(".ks-btn__primary");
+    await page.waitForTimeout(5000);
 }
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { defineConfig } from '@playwright/test';
-import { login, search, chips } from './helpers';
+import { loginFTTB, loginMSISDN, search, chips, buy } from './helpers';
 
 
 export default defineConfig({
@@ -12,9 +12,9 @@ export default defineConfig({
   ],
 });
 
-test.describe.only('hooks', ()=>{
+test.describe.only('FTTB', ()=>{
   test.beforeEach(async ({page})=>{
-    await login(page);
+    await loginFTTB(page);
   })
   test('onlineTv', async ({ page }) => {
     await page.click('.icon-close.text-16');
@@ -27,42 +27,62 @@ test.describe.only('hooks', ()=>{
   });
 
   test('contentGroupAssetPage', async ({page})=>{
+    const posterLocator = page.locator('.poster__media');
     await page.click('.icon-close.text-16');
     await page.click('text=Нове на Київстар ТБ');
-    await page.click('text=Дюна: Частина друга');
-    await page.click('text=Дивитись');
+    await page.click('.chips-container__item.ng-star-inserted >> text=За підпискою');
+    await page.waitForTimeout(2000);
+    await page.click('.chips-container__item.ng-star-inserted >> text=Покупка');
+    await page.waitForTimeout(2000);
+    await page.click('.chips-container__item.ng-star-inserted >> text=Доступно');
+    await page.waitForTimeout(2000);
+    await page.click(".sort-content-select.ng-star-inserted");
+    await page.click('text=За новизною');
+    await page.waitForTimeout(3000);
+    await page.click(".sort-content-select.ng-star-inserted");
+    await page.click('text=За рейтингом IMDB');
+    await page.waitForTimeout(3000);
+    await page.click(".sort-content-select.ng-star-inserted");
+    await page.click('text=За датою додавання');
+    await page.waitForTimeout(3000);
+    await page.click(".sort-content-select.ng-star-inserted");
+    await page.click('text=За популярністю');
+    await page.waitForTimeout(3000);
+    await posterLocator.first().click();
+    await page.click('.ks-btn__primary');
     await page.click('.relative.icon-volume');
     await page.waitForTimeout(5000);
   })
 
   test('search', async ({page})=>{
-    const channelName = '1+1 Україна HD';
-    const labelLocator = await page.locator(`label:text-is-exact("${channelName}")`);
+    const channelPoster = page.locator('.poster-wrapper.live'); 
     await search(page);
     await page.waitForTimeout(2000);
-    await page.click('.absolute.h-full.w-full.top-0.left-0.z-3');
+    await page.click('.absolute.h-full.w-full.top-0.left-0.z-3'); // відкривається ассет зі списку каналів
+    await page.click('.h-24');// назад на головну сторінку
+    await search(page);//повторний пошук
+    await page.click('.btn.advanced-search.rounded-none.w-full.h-40.absolute.bottom-0.m-0');//відкривається сторінка фільтрів
     await page.waitForTimeout(2000);
-    await page.click('.h-24');
+    await page.click('text=Телеканали');//обирається КГ тв канали
+    await channelPoster.first().click();//клік на перший тв канал у списку
+    await page.waitForTimeout(2000);
+    await page.click('.h-24');//назад на головну
     await search(page);
-    await page.click('.btn.advanced-search.rounded-none.w-full.h-40.absolute.bottom-0.m-0');
-    await page.waitForTimeout(2000);
-    await page.click('text=Телеканали');
-    await page.click('text="Єдині новини". Телемарафон');
-    await page.waitForTimeout(2000);
-    await page.click('.h-24');
-    await search(page);
-    await page.click('.btn.advanced-search.rounded-none.w-full.h-40.absolute.bottom-0.m-0');
-    await page.click('text=Фільтри');
-    await page.click('.whitespace-nowrap.ml-8.typo-body.text-gray-300 >> text=Фільми');
+    await page.click('.btn.advanced-search.rounded-none.w-full.h-40.absolute.bottom-0.m-0');//відкривається сторінка фільтрів
+    await page.click('text=Фільтри');//йдемо у фільтри
+    await page.click('.whitespace-nowrap.ml-8.typo-body.text-gray-300 >> text=Фільми');//обираємо фільми
     await page.waitForTimeout(3000);
-    await chips(page);
+    await chips(page);//проходимо по чіпсам
   })
 
   test('profileEdit', async ({page})=>{
+    const inputLocator = await page.locator('input[type=text]')
     await page.click(".w-32.h-32.rounded-full");
     await page.click("text= Персональні дані");
     await page.click(".ks-btn__ghost >> text=Редагувати");
-    await page.type("input[type=text]", "t267");
+    await inputLocator.fill('');
+    await page.waitForTimeout(2000);
+    await inputLocator.fill('t300');
     await page.click("text=Жінка");
     await page.click(".ks-btn__primary >> text=Зберегти");
     const pass = await page.locator(".typo-subtitle >> text=Налаштування пароля");
@@ -75,7 +95,7 @@ test.describe.only('hooks', ()=>{
     await page.click("text=Ввести промокод");
     await page.type("input[type=text]", "test04050Reusable");
     await page.click(".btn__new.btn.btn-primary.form__submit");
-    await page.click("text=Гаразд");
+    await page.click("text = Закрити");
     await page.click("text= Вийти з Київстар ТБ");
     await page.click(".ks-btn__primary.w-full.mb-40 >> text= Вийти з Київстар ТБ");
     await page.waitForTimeout(1000);
@@ -90,8 +110,51 @@ test.describe.only('hooks', ()=>{
     await page.waitForTimeout(2000);
     await page.goBack();
     await page.click("text=Весела сімейка");
-    await page.click('text=Дивитись');
-    await page.click(".clickable-element.open-popup-btn.relative");
+    await page.click('.ks-btn__primary');
+    await page.click('.icon-gear.clickable-element.relative');
+    await page.click('.title');
+    await page.click('.settings-form__label');
+    // await page.click(".clickable-element.open-popup-btn.relative");
+    // await page.click('/html/body/vd-root/div/div/ng-component/vd-asset-player/div[1]/div[2]/p/span');
     await page.waitForTimeout(5000);
+  })
+
+  test('myVideo', async ({page})=>{
+    await page.click(".btn.link.items-end");
+    await page.waitForTimeout(2000);
+    await page.click(".btn >> text= Обране ТБ");
+    await page.waitForTimeout(2000);
+    await page.click(".btn >> text= Куплене");
+    await page.waitForTimeout(2000);
+    await page.click(".btn >> text= Обране відео");
+    await page.waitForTimeout(2000);
+    await page.click(".btn >> text= Переглянуті");
+    await page.waitForTimeout(2000);
+  })
+
+  test('purchase',async ({page})=>{
+    await buy(page);
+  })
+
+  test('promoZone', async ({page})=>{
+    const promoImageLocator = page.locator('.ng-star-inserted');
+    await page.click('.icon-chevron-right');
+    await page.click('.icon-chevron-right');
+    await page.click('.icon-chevron-left')
+    await promoImageLocator.first().click();
+    await page.waitForTimeout(2000);
+  })
+})
+
+test.describe.only('MSISDN', ()=>{
+  test.beforeEach(async ({page})=>{
+    await loginMSISDN(page);
+  })
+
+  test('packagePage', async ({page})=>{
+    const packageRaw = page.locator('.cell');
+    await page.click(".btn.link >> text=Пакети");
+    await packageRaw.first().click();
+    await page.waitForTimeout(2000);
   })
 })
